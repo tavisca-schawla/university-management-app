a494be0b4f5c660edf7f8a42b0f13d2139de9d70
Deepak Shrivastav <dshrivastav@affinionds.com>
2019-09-10 16:17:46 +0530

    Basic-Student-Model Student-model Basic model comleted

---
+++ UniManager/UniManager.App/Models/Student/IStudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UniManager.App.Models.Student
+{
+    public interface IStudent
+    {
+        int RollNo { get; set; }
+        Course Course { get; set; }
+    }
+}
